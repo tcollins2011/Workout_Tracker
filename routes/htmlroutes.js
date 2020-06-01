ba7ab1e2ec63dacdd210d98739dd24e75bd27b4f
Tylre Collins <tyler@nhlp.com>
2020-06-01 07:34:51 -0600

    fixed broken cs after moving files

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = function(app){
     app.get('/', (req,res) => {
-        res.sendFile(path.join(__dirname, '../index.html'))
+        res.sendFile(path.join(__dirname, '../public/index.html'))
     })
 
     app.get('/exercise', (req,res) => {
